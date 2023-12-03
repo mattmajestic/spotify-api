c53ac9e7466d25973c83ca6845bab138ccf8be18
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2023-12-03 01:17:03 +0000

    youtube demo update

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import os
@@ -18,8 +19,11 @@ CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 # Initialize the Spotify API client
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
 
-@app.get("/get_artist/{artist_id}", response_class=JSONResponse)
-async def get_artist_stats(artist_id: str):
+# Initialize the Jinja2 templates
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/get_artist", response_class=JSONResponse)
+async def get_artist_stats(request: Request,artist_id: str = None):
     try:
         # Fetch artist data, including monthly listeners and images
         artist_data = sp.artist(artist_id)
@@ -33,44 +37,13 @@ async def get_artist_stats(artist_id: str):
             "images": images
         }
 
-        return JSONResponse(content=artist_info)
+        return templates.TemplateResponse("artist.html", {"request": request, "data": artist_info})
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"Error fetching artist statistics: {str(e)}")
 
-@app.get("/artist_page/{artist_id}", response_class=HTMLResponse)
-async def get_artist_page(artist_id: str):
-    try:
-        # Fetch artist data, including monthly listeners and images
-        artist_data = sp.artist(artist_id)
-        monthly_listeners = artist_data["followers"]["total"]
-        artist_name = artist_data["name"]
-        images = artist_data["images"]
-
-        # Generate the HTML page
-        html_content = f"""
-        <html>
-        <head>
-            <title>Artist Page: {artist_name}</title>
-        </head>
-        <body>
-            <h1>Artist: {artist_name}</h1>
-            <p>Monthly Listeners: {monthly_listeners}</p>
-            <h2>Images</h2>
-            <ul>
-        """
-
-        for image in images:
-            html_content += f'<li><img src="{image["url"]}" alt="Image" width="{image["width"]}" height="{image["height"]}"></li>'
-
-        html_content += """
-            </ul>
-        </body>
-        </html>
-        """
-
-        return HTMLResponse(content=html_content)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"Error fetching artist statistics: {str(e)}")
+@app.get("/", response_class=HTMLResponse)
+async def get_form(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
